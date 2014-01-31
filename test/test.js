3c69ee8387a755b70f685397f9584fdada528938
Mikola Lysenko <mikolalysenko@gmail.com>
2014-01-30 20:10:19 -0600

    adding test cases

--- test/test.js
+++ test/test.js
@@ -2,8 +2,12 @@
 
 var tape = require("tape")
 var dirichlet = require("../dirichlet")
+var almostEqual = require("almost-equal")
 
 tape("dirichlet", function(t) {
 
+  t.ok(almostEqual(dirichlet(10, 0), 1, almostEqual.DBL_EPSILON, almostEqual.DBL_EPSILON))
+  t.ok(almostEqual(dirichlet(10, 2), 0, almostEqual.DBL_EPSILON, almostEqual.DBL_EPSILON))
+  t.ok(almostEqual(dirichlet(10, 10), 1, almostEqual.DBL_EPSILON, almostEqual.DBL_EPSILON))
   t.end()
 })
